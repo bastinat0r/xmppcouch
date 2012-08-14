484ca091e1d650cfe614f3da6534bf5cf54f77e5
Sebastian Mai <bastinat0r@bastinat0r.de>
2012-08-14 10:07:31 +0000

    adding view to get data

--- create_db.js
+++ create_db.js
@@ -1,19 +1,46 @@
 var http = require('http');
 var util = require('util');
+var fs = require('fs');
 
 var opts = {
 	host : 'localhost',
 	port : 5984,
-	method: 'PUT'
+	method: 'PUT',
+	path : '/xmpp'
 }
 
-opts.path = '/xmpp';
-
-var req = http.request(opts, function(res) {
-	res.on('data', function(data) {
-		util.puts(data);
+function createDB(opts, cb) {
+	
+	var req = http.request(opts, function(res) {
+		res.on('data', function(data) {
+			util.puts(data);
+		});
 	});
-});
 
-req.end();
+	req.end();
+	cb();
+};
 
+
+function putDesign() {
+	opts.path = opts.path + '/_design/all'
+	var req = http.request(opts, function(res) {
+		res.on('data', function(data) {
+			util.puts(data);
+		});
+	});
+
+	fs.readFile('db/_design/all.json', 'utf8', function(err, data) {
+		if(err) {
+			util.puts(err);
+			req.end();
+		}
+		else {
+			util.puts(data);
+			req.write(data);
+			req.end();
+		}
+	});
+}
+
+createDB(opts, putDesign);
