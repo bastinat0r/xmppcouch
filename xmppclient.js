8b39a21883b825604e3b79a22218ab5397102836
Sebastian Mai <bastinat0r@bastinat0r.de>
2012-08-15 12:47:03 +0200

    moving couch-config to config.js

--- xmppclient.js
+++ xmppclient.js
@@ -3,37 +3,37 @@ var util = require('util');
 var http = require('http');
 var config = require('./config');
 
-var couchOpts = {
-	host: 'localhost',
-	port: '5984',
-}
-
 function putDB (dbObject) {
-	var opts = couchOpts;
+	var opts = config.couch;
 	opts.method = 'GET';
 	opts.path = '/_uuids';
+	
 	var req = http.request(opts, function(res) {
 		res.on('data', function(data) {
-		var uuid = JSON.parse(data).uuids[0];
-		var opts = couchOpts;
+			var uuid = JSON.parse(data).uuids[0];
+		
+			var opts = config.couch;
 			opts.path = '/xmpp/'+ uuid;
 			opts.method = 'PUT';
 			var req = http.request(opts, function(res) {
-			req.on('error', function (err) {
-				util.puts(err);
-			});
 			res.on('data', function(data) {
 				util.puts(data);
 				});
+			});
+
+			req.on('error', function (err) {
+				util.puts(err);
 			});
 			req.write(JSON.stringify(dbObject));
 			req.end();	
 		});
 	});
+
 	req.on('error', function(err) {
 		util.puts(err);
 		util.puts('couchdb up and running?');
 	});
+
 	req.end();
 };
 
